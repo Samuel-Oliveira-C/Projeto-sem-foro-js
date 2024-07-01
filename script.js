149bf573a97233e73492ceb35d18c98eeacd1ac0
Samuel Oliveira <samuel.oleiv@gmail.com>
2024-07-01 17:34:44 -0300

    Feature(html, CSS(interno) e js externo) : Criando a parte inicial do projeto

---
+++ script.js
@@ -0,0 +1,14 @@
+/*Criar um Semaforo de transito
+    o que eu preciso:
+    foto de semaforo igual
+    vermelho
+    amarelo
+    verde
+-----------------------------------
+Lógica:
+    dentro de um determinado tempo um dos três sinais irá aparecer (Assincronismo)
+------------------------------------------------------------------------------------------
+Style ?
+
+*/
+
